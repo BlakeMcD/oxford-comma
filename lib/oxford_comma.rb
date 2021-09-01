0d65e9115b754e7de037f94b63d838b7cb71e1e6
BlakeMcD <blakemcd@gmail.com>
2021-09-01 16:13:46 +1000

    assignment passed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif  array.length == 2
+        array.join(" and ")
+    else 
+        last_value = array.last
+        last_value_replacement = "and "+last_value
+        array.pop
+        array << last_value_replacement
+        array.join(", ")
+    end
 end
